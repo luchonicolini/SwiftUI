6c1516a972e1bb99e150297583e36982f2276bf0
luciano <nicolini.luciano@hotmail.com>
2021-05-13 06:05:30 -0300

    Snake / Juego
    Snake / Juego echo en SwiftUI. 
    Dificultad/Medio
    Cuenta con las características de un juego base.

---
+++ Snake/Snake/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Snake
+//
+//  Created by Luciano Nicolini on 11/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
