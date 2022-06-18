70da43709812604f0b395fa193dbf82a28eef3fb
luciano <nicolini.luciano@hotmail.com>
2022-06-18 00:50:18 -0300

    Moonshot SwiftUI Tutorial
    Merging Codable structs – Moonshot SwiftUI Tutorial -  https://www.youtube.com/watch?v=4CIVfa5dRNE

---
+++ Moon/Shared/MoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonApp.swift
+//  Shared
+//
+//  Created by Luciano Nicolini on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
